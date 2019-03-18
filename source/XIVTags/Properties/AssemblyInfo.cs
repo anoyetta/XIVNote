5d2314d42f0a65e61cfed168c2bf931f4115ce4a
anoyetta <anoyetta@gmail.com>
2019-03-18 18:04:23 +0900

    first commit

---
+++ source/XIVTags/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("XIVTags")]
+[assembly: AssemblyDescription("https://github.com/anoyetta/XIVTags")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XIVTags")]
+[assembly: AssemblyCopyright("Copyright © anoyetta 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly
+)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
